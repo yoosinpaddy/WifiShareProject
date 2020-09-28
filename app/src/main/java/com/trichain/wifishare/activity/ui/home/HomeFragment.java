9c8ab967a68e8488f12251fc27431c8066a4c275
Yoosin Paddy <42375733+yoosinpaddy@users.noreply.github.com>
2020-09-28 23:01:30 +0300

    Firebase Functionality

--- app/src/main/java/com/trichain/wifishare/activity/ui/home/HomeFragment.java
+++ app/src/main/java/com/trichain/wifishare/activity/ui/home/HomeFragment.java
@@ -12,7 +12,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.trichain.wifishare.activity.R;
+import com.trichain.wifishare.R;
+
 
 public class HomeFragment extends Fragment {
 
