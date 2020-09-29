4b219e90152fafe33e5a1823016268b1deb84678
Noblepal <zeronillzero@gmail.com>
2020-09-29 11:11:24 +0300

    Added countries select activity

--- app/src/main/java/com/trichain/wifishare/fragment/ConnectFragment.java
+++ app/src/main/java/com/trichain/wifishare/fragment/ConnectFragment.java
@@ -1,23 +1,33 @@
 package com.trichain.wifishare.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.trichain.wifishare.R;
+import com.trichain.wifishare.activity.CountrySelectActivity;
+import com.trichain.wifishare.databinding.FragmentConnectBinding;
 
 
 public class ConnectFragment extends Fragment {
 
+    private FragmentConnectBinding b;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_connect, container, false);
+        b = DataBindingUtil.inflate(inflater, R.layout.fragment_connect, container, false);
 
-        return root;
+        b.btnSelectCountry.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), CountrySelectActivity.class));
+        });
+
+        return b.getRoot();
     }
 }
