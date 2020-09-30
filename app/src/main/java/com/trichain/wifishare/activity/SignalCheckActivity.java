58863e6f1f944151ad0d3f4650651bfff820ab0b
Noblepal <zeronillzero@gmail.com>
2020-09-30 16:38:33 +0300

    Signal strength

--- app/src/main/java/com/trichain/wifishare/activity/SignalCheckActivity.java
+++ app/src/main/java/com/trichain/wifishare/activity/SignalCheckActivity.java
@@ -1,16 +1,58 @@
 package com.trichain.wifishare.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.trichain.wifishare.R;
+import com.trichain.wifishare.databinding.ActivitySignalCheckBinding;
+import com.trichain.wifishare.util.CheckConnectivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SignalCheckActivity extends AppCompatActivity {
+
+    private ActivitySignalCheckBinding b;
+    private static final String TAG = "SignalCheckActivity";
+    private static final float MAX_STRENGTH = 5.0f;
+    private Timer timer;
+    private int connectionStrength = 0;
+    private float percent = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signal_check);
+        b = DataBindingUtil.setContentView(this, R.layout.activity_signal_check);
+
+        Log.e(TAG, "onCreate: " + CheckConnectivity.getSingleWiFiSignalStrength(this));
+
+        startConnectionLoop();
+
+    }
+
+    private void startConnectionLoop() {
+        Log.e(TAG, "startConnectionLoop: Attempting to start signal check");
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                connectionStrength = CheckConnectivity.getSingleWiFiSignalStrength(SignalCheckActivity.this);
+                percent = (connectionStrength / MAX_STRENGTH) * 100.0f;
+                Log.e(TAG, "run: Signal " + connectionStrength);
+                Log.e(TAG, "run: MAX_STRENGTH " + MAX_STRENGTH);
+                Log.e(TAG, "run: percent " + percent);
+                runOnUiThread(() -> {
+                    b.awesomeSpeedometer.realSpeedTo(percent);
+                });
+            }
+        }, 1000, 500);
+
+        new Handler().postDelayed(() -> {
+            timer.cancel();
+        }, 10000);
     }
 }
--- app/src/main/java/com/trichain/wifishare/util/CheckConnectivity.java
+++ app/src/main/java/com/trichain/wifishare/util/CheckConnectivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 
 public class CheckConnectivity {
@@ -45,5 +47,13 @@ public class CheckConnectivity {
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         return (networkInfo != null && networkInfo.isConnected());
+    }
+
+
+    public static int getSingleWiFiSignalStrength(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        int numberOfLevels = 5;
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        return WifiManager.calculateSignalLevel(wifiInfo.getRssi(), numberOfLevels);
     }
 }
