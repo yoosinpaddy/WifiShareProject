e42164fe5cf78cc1bbc9d39ce8b8bd1107560bbb
Yoosin Paddy <yoosinfrancis@gmail.com>
2020-10-15 20:10:26 +0300

    Final

--- app/src/main/java/com/trichain/wifishare/activity/CancelSharingActivity.java
+++ app/src/main/java/com/trichain/wifishare/activity/CancelSharingActivity.java
@@ -6,13 +6,29 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.trichain.wifishare.R;
 import com.trichain.wifishare.databinding.ActivityCancelSharingBinding;
+import com.trichain.wifishare.model.WifiModel;
+import com.trichain.wifishare.util.CheckConnectivity;
 
 public class CancelSharingActivity extends AppCompatActivity {
     ActivityCancelSharingBinding b;
+    private static final String WIFI_ROOT = "wifi_locations";
+    boolean canceled=false;
+    private static final String TAG = "CancelSharingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +36,65 @@ public class CancelSharingActivity extends AppCompatActivity {
         b = DataBindingUtil.setContentView(this, R.layout.activity_cancel_sharing);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         new Handler().postDelayed(runnable, 15000);
+        cancelWifiSharing(() -> {
+            canceled=true;
+            Toast.makeText(this, "Canceling done", Toast.LENGTH_SHORT).show();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    CancelSharingActivity.super.onBackPressed();
+                }
+            },1500);
+        });
     }
-    Runnable runnable=new Runnable() {
+
+    Runnable runnable = new Runnable() {
         @Override
         public void run() {
             runOnUiThread(() -> {
-                b.retry.setText("Retry");
-                b.retry.setOnClickListener(v -> {
-                    b.retry.setText("Cancelling...");
-                    new Handler().postDelayed(runnable, 15000);
-                });
+                if (!canceled){
+                    b.retry.setText("Retry");
+                    b.retry.setOnClickListener(v -> {
+                        b.retry.setText("Cancelling...");
+                        new Handler().postDelayed(runnable, 15000);
+                    });
+                }
             });
         }
     };
+
+    interface Canceling {
+        void onComplete();
+    }
+
+    public void cancelWifiSharing(Canceling c) {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(WIFI_ROOT);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot a : dataSnapshot.getChildren()) {
+                    WifiModel wifiModel = a.getValue(WifiModel.class);
+                    if (wifiModel != null) {
+                        if (wifiModel.getSsid().replace("\"", "").contentEquals(CheckConnectivity.getWiFiName(CancelSharingActivity.this).replace("\"", ""))) {
+                            Log.e(TAG, "onDataChange: deleting " + wifiModel.getSsid());
+                            if (a.getKey() != null) {
+                                ref.child(a.getKey()).removeValue();
+                            }
+                        }
+                    }
+                    Log.e(TAG, "onDataChange: " + wifiModel.toString());
+                }
+                c.onComplete();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
--- app/src/main/java/com/trichain/wifishare/activity/SpeedCheckActivity.java
+++ app/src/main/java/com/trichain/wifishare/activity/SpeedCheckActivity.java
@@ -1,6 +1,7 @@
 package com.trichain.wifishare.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -27,8 +28,10 @@ public class SpeedCheckActivity extends AppCompatActivity {
     private ActivitySpeedCheckBinding b;
     private static final String TAG = "SpeedCheckActivity";
     private SpeedTestSocket speedTestSocket;
+    private SpeedTestSocket speedTestSocket2;
     private AppExecutors appExecutors;
     private boolean isDownloadTest = true;
+    float upload = 0;
 
 
     @Override
@@ -42,12 +45,15 @@ public class SpeedCheckActivity extends AppCompatActivity {
         appExecutors = AppExecutors.getInstance();
 
         speedTestSocket = new SpeedTestSocket();
+        speedTestSocket2 = new SpeedTestSocket();
 
-        b.tvWiFiNameSpeed.setText(CheckConnectivity.getWiFiName(this).replace("\"",""));
+        b.tvWiFiNameSpeed.setText(CheckConnectivity.getWiFiName(this).replace("\"", ""));
         b.tvDownloadSpeed.setText(getString(R.string.string_string, "Download: ", "0Kbps"));
         b.tvUpSpeed.setText(getString(R.string.string_string, "Upload:   ", "0Kbps"));
         b.speedView.setSpeedometerMode(Speedometer.Mode.NORMAL);
         b.speedView.setWithTremble(false);
+        b.speedViewUpload.setSpeedometerMode(Speedometer.Mode.NORMAL);
+        b.speedViewUpload.setWithTremble(false);
 
         // add a listener to wait for speedtest completion and progress
         speedTestSocket.addSpeedTestListener(new ISpeedTestListener() {
@@ -59,15 +65,12 @@ public class SpeedCheckActivity extends AppCompatActivity {
                 System.out.println("[COMPLETED] rate in octet/s : " + report.getTransferRateOctet());
                 System.out.println("[COMPLETED] rate in bit/s   : " + report.getTransferRateBit());
                 appExecutors.mainThread().execute(() -> {
-                    b.speedView.speedTo(0, 1500);
 
-                    if (isDownloadTest) {
-                        b.tvDownloadSpeed.setText(getString(R.string.string_string, "Download: ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
-                        isDownloadTest = false;
-                        testUploadSpeed();
-                    } else {
-                        b.tvUpSpeed.setText(getString(R.string.string_string, "Upload:   ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
-                    }
+                    //b.speedView.speedTo(0, 1500);
+                    b.tvDownloadSpeed.setText(getString(R.string.string_string, "Download: ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
+                    isDownloadTest = false;
+
+                    //testUploadSpeed();
 
                 });
 
@@ -85,20 +88,63 @@ public class SpeedCheckActivity extends AppCompatActivity {
 
             @Override
             public void onProgress(float percent, SpeedTestReport report) {
-                Log.e(TAG, "onProgress: [Progress] " + percent);
+                Log.e(TAG, "onProgress: [ Progress%] " + percent);
+                Log.e(TAG, "onProgress: [ Progress] " + Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000);
                 // called to notify download/upload progress
-                System.out.println("[PROGRESS] progress : " + percent + "%");
+                System.out.println("[PROGRESS]  progress : " + percent + "%");
                 appExecutors.mainThread().execute(() -> {
-                    if (isDownloadTest) {
-                        b.speedView.speedTo(Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000, 1000);
-                        b.tvDownloadSpeed.setText(getString(R.string.string_string, "Download: ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
-                    } else {
-                        Log.e(TAG, "onProgress: upload");
-                        b.speedView.speedTo(Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000, 1000);
-                        b.tvUpSpeed.setText(getString(R.string.string_string, "Upload:   ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
-                    }
+                    b.speedView.speedTo(Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000, 1000);
+                    b.tvDownloadSpeed.setText(getString(R.string.string_string, "Download: ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
+
 
                 });
+                System.out.println("[PROGRESS] rate in octet/s : " + report.getTransferRateOctet());
+                System.out.println("[PROGRESS] rate in bit/s   : " + report.getTransferRateBit());
+            }
+        });
+        // add a listener to wait for speedtest completion and progress
+        speedTestSocket2.addSpeedTestListener(new ISpeedTestListener() {
+
+            @Override
+            public void onCompletion(SpeedTestReport report) {
+                Log.e(TAG, "onCompletion: [Complete] ");
+                // called when download/upload is complete
+                System.out.println("[COMPLETED] rate in octet/s : " + report.getTransferRateOctet());
+                System.out.println("[COMPLETED] rate in bit/s   : " + report.getTransferRateBit());
+                appExecutors.mainThread().execute(() -> {
+                   // b.speedViewUpload.speedTo(0, 1500);
+                    b.tvUpSpeed.setText(getString(R.string.string_string, "Upload:   ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
+                });
+              }
+
+
+            @Override
+            public void onError(SpeedTestError speedTestError, String errorMessage) {
+                // called when a download/upload error occur
+                Log.e(TAG, "onError: Network Error occurred -> " + errorMessage);
+
+                appExecutors.mainThread().execute(() -> {
+                    b.speedViewUpload.speedTo(0, 1500);
+                });
+            }
+
+            @Override
+            public void onProgress(float percent, SpeedTestReport report) {
+                Log.e(TAG, "onProgress: [Upload Progress%] " + percent);
+                Log.e(TAG, "onProgress: [Upload Progress] " + Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000);
+                // called to notify download/upload progress
+                System.out.println("[PROGRESS] Upload progress : " + percent + "%");
+                if (upload != 0 && Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000 < 50) {
+                        appExecutors.mainThread().execute(() -> {
+                            Log.e(TAG, "onProgress: upload");
+                            b.speedViewUpload.speedTo(Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000, 1000);
+                            b.tvUpSpeed.setText(getString(R.string.string_string, "Upload:   ", Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000 + "Kbps"));
+
+
+                        });
+
+                }
+                upload = Float.parseFloat(String.valueOf(report.getTransferRateBit().toBigInteger())) / 1000000;
                 System.out.println("[PROGRESS] rate in octet/s : " + report.getTransferRateOctet());
                 System.out.println("[PROGRESS] rate in bit/s   : " + report.getTransferRateBit());
             }
@@ -120,7 +166,7 @@ public class SpeedCheckActivity extends AppCompatActivity {
 
         appExecutors.networkIO().execute(() -> {
             String fileName = SpeedTestUtils.generateFileName() + ".txt";
-            speedTestSocket.startFixedUpload("ftp://speedtest.tele2.net/upload/" + fileName, 10000000, 18000);
+            speedTestSocket2.startFixedUpload("ftp://speedtest.tele2.net/upload/" + fileName, 10000000, 18000);
             //speedTestSocket.startUpload("http://ipv4.ikoula.testdebit.info/", 1000000);
         });
     }
@@ -157,8 +203,16 @@ public class SpeedCheckActivity extends AppCompatActivity {
 
         b.tvDownloadSpeed.setText(getString(R.string.string_string, "Download: ", "0Kbps"));
         b.tvUpSpeed.setText(getString(R.string.string_string, "Upload:   ", "0Kbps"));
-isDownloadTest=true;
+        isDownloadTest = true;
+        testUploadSpeed();
         testDownloadSpeed();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                testDownloadSpeed();
+            }
+        }, 1500);
+//        testDownloadSpeed();
 
     }
 
